542a943ca3a878ca212f6f89319098b1903c55c7
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-01 17:34:35 +0000

    Add files via upload

--- myApp.py
+++ myApp.py
@@ -3,10 +3,14 @@ from io import StringIO
 import pandas as pd
 import streamlit as st
 
-my_csv = st.file_uploader("Upload csv", type="csv")
 
-if my_csv is not None:
-    st.session_state["my_csv"] = my_csv.getvalue().decode("utf-8")
+col1, col2 = st.columns(2)
 
-if "my_csv" in st.session_state:
-    st.sidebar.dataframe(pd.read_csv(StringIO(st.session_state["my_csv"])))
+with col1:
+    my_csv = st.file_uploader("Upload csv", type="csv")
+
+with col2:
+    if my_csv is not None:
+        st.session_state["my_csv"] = my_csv.getvalue().decode("utf-8")
+    if "my_csv" in st.session_state:
+        st.sidebar.dataframe(pd.read_csv(StringIO(st.session_state["my_csv"])))
