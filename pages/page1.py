f324274fed7f261f934dba21c33d9867db36bfc6
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-01 17:40:54 +0000

    Add files via upload

--- pages/page1.py
+++ pages/page1.py
@@ -7,3 +7,5 @@ if my_image is not None:
 
 if "my_image" in st.session_state:
     st.sidebar.image(st.session_state["my_image"])
+if "my_csv" in st.session_state:
+    st.dataframe(pd.read_csv(StringIO(st.session_state["my_csv"])))
